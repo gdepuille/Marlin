3d3f0d8881b649f1174ed198acd3652a67279acc
Gregory DEPUILLE <gregory.depuille@gmail.com>
2026-05-17 01:33:27 +0200

    Safe Homing Y on center

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2208,7 +2208,7 @@
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // (mm) X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT 4         // (mm) Y point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // (mm) Y point for Z homing
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
