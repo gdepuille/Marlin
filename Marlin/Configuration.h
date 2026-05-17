0ef7c6fe933f1fd04d58dd3ba70058694a33b42c
Gregory DEPUILLE <gregory.depuille@gmail.com>
2026-05-18 00:22:34 +0200

    BLTouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1582,7 +1582,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, -8, -3 }
+#define NOZZLE_TO_PROBE_OFFSET { -38, -8, -0.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -2072,7 +2072,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 8
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
